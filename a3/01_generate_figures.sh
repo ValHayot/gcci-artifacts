12ac6643bcb425ca48309557a88b98f98aa50734
valhayot <valeriehayot@gmail.com>
2025-07-15 20:53:49 -0400

    Fix bug with generating a3 figures

--- a3/01_generate_figures.sh
+++ a3/01_generate_figures.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker compose run --rm -v a3/a3_plot.ipynb:a3_plot.ipynb -v a3/data:data parsldock jupyter execute a3_plot.ipynb
+docker compose run --rm \
+        -v $PWD/a3/a3_plot.ipynb:/home/cc/parsldock/a3_plot.ipynb \
+        -v $PWD/a3/data:/home/cc/parsldock/data \
+        -v $PWD/a3/figure:/home/cc/parsldock/figure parsldock jupyter execute a3_plot.ipynb
