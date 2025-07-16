2fb3711b9fd82ce096e12d3cf6f44f2ad08b6659
valhayot <valeriehayot@gmail.com>
2025-07-15 20:41:33 -0400

    Fix TTY bug

--- a3/00_build_and_start_endpoint.sh
+++ a3/00_build_and_start_endpoint.sh
@@ -2,4 +2,4 @@
 
 docker compose build parsldock
 docker compose up parsldock --detach
-cat a3/get_endpoint_uuid.sh | docker compose exec -i parsldock /bin/bash -s
+cat a3/get_endpoint_uuid.sh | docker compose exec -T parsldock /bin/bash -s
--- a4/00_build_and_start_endpoint.sh
+++ a4/00_build_and_start_endpoint.sh
@@ -2,4 +2,4 @@
 
 docker compose build psij
 docker compose up psij --detach
-cat a4/get_endpoint_uuid.sh | docker compose exec -i psij /bin/bash -s
+cat a4/get_endpoint_uuid.sh | docker compose exec -T psij /bin/bash -s
